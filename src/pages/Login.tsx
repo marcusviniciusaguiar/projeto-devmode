ba8d05f4748b87da60265fa67bbfda71c264b2c2
Marcus Vinícius Aguiar <marcusviniciusaguiarsoares@gmail.com>
2026-06-15 20:56:30 -0300

    feat: cria formulário de login com campos controlados

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,32 @@
+import React, { useState } from "react";
+
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    }
+
     return (
         <>
         <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+            <label>Senha</label>
+            <input type="password" onChange={handlePassword} value={password} />
+            <button>Entrar</button>
+        </form>
         </>
     )
 }
