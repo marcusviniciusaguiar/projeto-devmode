ce5102dae939600b358bd83579bda1105b9f23df
Marcus Vinícius Aguiar <marcusviniciusaguiarsoares@gmail.com>
2026-06-18 00:37:41 -0300

    feat: header com nome do usuário e logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
 function Header() {
+
+    const { user, logout } = useAuth();
+
     return(
         <>
         <Link to={"/"}>Ir para a Home </Link>
-        <Link to={"/login"}>Logar no sistema</Link>
+        { user ? (
+            <nav>
+                <span>{user.name}</span>
+                <Link to={"/profile/edit"}>Editar perfil</Link>
+                <button onClick={() => logout()}>Sair</button>
+            </nav>
+            ) : (
+            <nav>
+                <Link to={"/login"}>Fazer login</Link>
+                <Link to={"/register"}>Registrar-se</Link>
+            </nav>
+            )
+        }
+        
         </>
     )
 }
